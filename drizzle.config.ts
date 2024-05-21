759ccb0fb701ea0a13bc20a9536c59aafaa81217
Nam Nguyen <ducnam1719@gmail.com>
2024-05-22 06:12:54 +0700

    feat: config db with drizzle & neon

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, type Config } from 'drizzle-kit';
+import { cwd } from 'node:process';
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(cwd());
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './db/schema.ts',
+  out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} as Config);
